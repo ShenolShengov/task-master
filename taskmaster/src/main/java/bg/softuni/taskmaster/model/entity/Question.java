b6e82a6e362febcb9cb9fd07eb5ecd0b9931b9d5
Shenol07 <shenolshengov41@gmail.com>
2024-07-18 22:33:35 +0300

    i18n login page

--- taskmaster/src/main/java/bg/softuni/taskmaster/model/entity/Question.java
+++ taskmaster/src/main/java/bg/softuni/taskmaster/model/entity/Question.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "questions")
