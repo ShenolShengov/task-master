d600b49f85457c391aff28f4c8b97572e762e7f3
Shenol07 <shenolshengov41@gmail.com>
2024-08-16 12:10:18 +0300

    Add ResponseStatus to custom exceptions

--- taskmaster/src/main/java/bg/softuni/taskmaster/exceptions/ObjectNotFoundException.java
+++ taskmaster/src/main/java/bg/softuni/taskmaster/exceptions/ObjectNotFoundException.java
@@ -1,8 +1,11 @@
 package bg.softuni.taskmaster.exceptions;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Object not found")
 public class ObjectNotFoundException extends RuntimeException {
 
     private final String type;
--- taskmaster/src/main/java/bg/softuni/taskmaster/exceptions/TaskNotFoundException.java
+++ taskmaster/src/main/java/bg/softuni/taskmaster/exceptions/TaskNotFoundException.java
@@ -1,5 +1,9 @@
 package bg.softuni.taskmaster.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Task not found")
 public class TaskNotFoundException extends ObjectNotFoundException {
 
     public TaskNotFoundException() {
